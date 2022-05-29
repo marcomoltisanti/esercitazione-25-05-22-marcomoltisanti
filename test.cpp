7cc52aa4e748f33b2690249b5dfdefc1727fe30a
marcomoltisanti <moltisanti.m@gmail.com>
2022-05-29 10:30:39 +0200

    Add test sottrazione

--- test.cpp
+++ test.cpp
@@ -1,11 +1,19 @@
 #include "include/operazione_aritmetica.h"
 #include "include/addizione.h"
+#include "include/sottrazione.h"
 
 void test_addizione() {
 	Addizione a(4.5, 4.5);
 	std::cout << a.eseguiCalcolo() << std::endl;
 }
 
+
+void test_sottrazione() {
+	Sottrazione s(5.3, 4.5);
+	std::cout << s.eseguiCalcolo() << std::endl;
+}
+
 int main() {
 	test_addizione();
+	test_sottrazione();
 }
